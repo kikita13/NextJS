c89a4ce4317fbf682dc0ed7dd60cf39b40bc586b
Nikita <qazxswedcxzaqw6@gmail.com>
2022-07-22 19:29:37 +0800

    fix style Not important

--- components/Layout/AppBarItems/AppBar.jsx
+++ components/Layout/AppBarItems/AppBar.jsx
@@ -6,7 +6,6 @@ import { useWindowSize } from '@consts/hooks';
 export const AppBar = () => {
 
   const size= useWindowSize();
-  console.log(size)
 
   return size.width > 400 ? <NavMain styles={styles}/> : <NavDrawer styles={styles}/>
     
